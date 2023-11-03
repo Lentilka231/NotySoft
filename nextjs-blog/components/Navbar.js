903c66da7e0c76af6679dc9d2a8e27d3e29012ae
Lentilka231 <jakesradim45@seznam.cz>
2023-11-03 14:05:02 +0100

    Transfering from notebook to Desktop

--- nextjs-blog/components/Navbar.js
+++ nextjs-blog/components/Navbar.js
@@ -1,9 +1,10 @@
 import styles from "../stylesheets/Navbar.module.css"
+import NotesToolConatiner from "./NotesToolConatiner"
 export default function Navbar(){
     return (
         <>
         <header  className={styles.NavBar}>
-
+            <NotesToolConatiner/>
         </header>
         </>
     )
---
+++ nextjs-blog/components/NotesToolConatiner.js
@@ -0,0 +1,9 @@
+import styles from "../stylesheets/Navbar.module.css"
+
+export default function NotesToolConatiner (){
+    return (
+        <div className={styles.toolContainer}>
+            <SVG src="" />
+        </div>
+    );
+}
